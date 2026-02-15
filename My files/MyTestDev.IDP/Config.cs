32b1833e81168c5916c9a16d1b399181b71add56
Mirosław Kokociński <mkspace08@gmail.com>
2026-02-15 19:05:44 +0100

    Idp added. UI razorpages. Basic test config done

---
+++ My files/MyTestDev.IDP/Config.cs
@@ -0,0 +1,44 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using static System.Net.WebRequestMethods;
+
+namespace MyTestDev.IDP;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+            { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[] 
+            {
+                new Client()
+                {
+                    ClientName = "Image Gallery",
+                    ClientId = "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris =
+                    {
+                        "https://localhost:7184/signin-oidc"  //signin-oidc - it is default value
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("ClientSecret123".Sha256())
+                    }
+                    
+                }
+            };
+}
